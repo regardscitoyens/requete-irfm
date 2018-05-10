d3d67c447dae847329d7b9183c7a79047dd72b5f
Tangui Morlier <tangui@tangui.eu.org>
2018-05-10 17:30:19 +0200

    Avec adresse

--- bin/create_md.py
+++ bin/create_md.py
@@ -7,6 +7,7 @@ env = Environment(loader=FileSystemLoader("."))
 
 template = env.get_template(sys.argv[1])
 print template.render(depute='Regis Lateur',
+                        depute_adresse="Assemblee nationale, 126 rue de l'universite, 75355 Paris 07 SP",
                         cada_no='2017XXXXX',
                         cada_date='1 janvier 2018',
                         refus_date='19 juin 2017',
